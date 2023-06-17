d164b3e4cfcdbabf660cc6395fff0aa7982181ca
Elsayed Hamada <elsayed.7work@gmail.com>
2023-06-18 01:58:59 +0300

    Finish Media Query

---
+++ script.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector(".menu-container");
+const menuIcon = document.querySelector(".menu");
+
+const navList = document.querySelector(".list-1");
+
+menu.addEventListener("click", () => {
+  navList.classList.toggle("active");
+  if (navList.classList.contains("active")) {
+    menuIcon.className = "fa-solid fa-xmark menu";
+  } else {
+    menuIcon.className = "fa-solid fa-bars menu";
+  }
+});
