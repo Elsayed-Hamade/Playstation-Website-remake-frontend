02b9439867bd32ca6e83267a181153f092743f9d
Elsayed Hamada <elsayed.7work@gmail.com>
2023-06-21 00:39:39 +0300

    smooth scroll

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // Menu Func.
 const menu = document.querySelector(".menu-container");
 const menuIcon = document.querySelector(".menu");
-
 const navList = document.querySelector(".list-1");
 
 menu.addEventListener("click", () => {
@@ -17,6 +16,7 @@ menu.addEventListener("click", () => {
 // Log Close, open
 const logBtn = document.querySelectorAll(".log-btn-activator");
 const logContainer = document.querySelector(".logIn-container");
+const logBackground = document.querySelector(".log-background");
 const LogClose = document.querySelector(".log-close");
 
 logBtn.forEach((btn) => {
@@ -25,6 +25,48 @@ logBtn.forEach((btn) => {
   });
 });
 
-LogClose.addEventListener("click", () => {
-  logContainer.style.display = "none";
+const closeFunc = () => (logContainer.style.display = "none");
+LogClose.addEventListener("click", closeFunc);
+logBackground.addEventListener("click", closeFunc);
+
+// Cookie
+const cookie = document.createElement("div");
+cookie.classList.add("cookie");
+cookie.innerHTML = `
+<p class="cookie-text">
+we use cookie for improving your user experience
+</p>
+<button class="btn-2 cookie-btn">Got it!</button>
+`;
+document.body.prepend(cookie);
+
+const cookieBtn = document.querySelector(".cookie-btn");
+cookieBtn.addEventListener("click", () => {
+  cookie.remove();
 });
+
+// scrolling
+const home = document.querySelector(".home");
+const toHome = document.querySelectorAll(".to-home");
+const hardware = document.querySelector(".hardware");
+const toHardware = document.querySelectorAll(".to-hardware");
+const games = document.querySelector(".games");
+const toGames = document.querySelectorAll(".to-games");
+const shop = document.querySelector(".shop");
+const toShop = document.querySelectorAll(".to-shop");
+const support = document.querySelector(".footer");
+const toSupport = document.querySelectorAll(".to-support");
+
+const scrollFunc = (btns, section) => {
+  btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+};
+
+scrollFunc(toHome, home);
+scrollFunc(toHardware, hardware);
+scrollFunc(toGames, games);
+scrollFunc(toShop, shop);
+scrollFunc(toSupport, support);
